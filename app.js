f5b120da4f5ec9112fe76732da45a6b4234ed66c
razexay <44016866+razexay@users.noreply.github.com>
2020-01-31 13:39:42 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const btns = Array.prototype.slice.call(document.querySelectorAll(".btn"));
+const answer = document.querySelector("#answer");
+
+btns.map(btn => btn.addEventListener("click", (evt) => {
+    answer.innerText = evt.target.innerText
+}));
